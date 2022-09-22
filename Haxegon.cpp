f973e01cabc13aa25245ac0f5cca36e501dfa08f
Muzamil Zaman Khan <95095602+MuzamilDev@users.noreply.github.com>
2022-09-22 19:26:33 +0500

    Add files via upload

---
+++ Haxegon.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+    float Area,x;
+
+    cout<<"Enter The length of Hexagon: "<<endl;
+    cin>>x;
+
+    Area=3*(sqrt(3))*2*x/2;
+
+    cout<<"The Area is: "<<Area<<endl;
+
+    return 0;
+}
